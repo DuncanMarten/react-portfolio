bdab6e928eacd2adbc59aea43fb97e8631293a1f
Duncan <duncanmarten@yahoo.com>
2021-01-28 09:03:34 -0600

    created header with nav and a footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+
+    return (
+        <footer>
+            <ul>
+                <li>
+                <a href="https://github.com/DuncanMarten" target="_blank" rel="noreferrer">Github</a>
+                </li>
+                <li>
+                <a href="https://www.linkedin.com/in/duncan-marten-61210b1b5/" target="_blank" rel="noreferrer">LinkedIn</a>
+                </li>
+                <li>
+                <a href="https://open.spotify.com/user/1291323689?si=j32bG78GRRCqT2HECsYYZw" target="_blank" rel="noreferrer">Spotify</a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from "../Navigation";
+
+function Header() {
+
+    return (
+        <header>
+            <h2>
+                Duncan Marten
+            </h2>
+            <Navigation />
+        </header>
+    )
+}
+
+export default Header;
